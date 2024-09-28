1e610ae81974ae3505614a3c1ae9bbe1e4ff3893
Vaghani Dakshil <119387136+dakshilvaghani@users.noreply.github.com>
2024-09-28 11:19:26 +0530

    Update authenticateUser.js

--- backend/middlewares/authenticateUser.js
+++ backend/middlewares/authenticateUser.js
@@ -2,18 +2,17 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 const authenticateUser = async (req, res, next) => {
-  const authHeader = req.headers.authorization; // Get the Authorization header
-  const token = authHeader && authHeader.split(" ")[1]; // Get the token from Bearer
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
 
   console.log(token);
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Replace with your secret key
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded);
 
-    // Use 'userId' from the decoded token
-    req.user = await User.findById(decoded.userId); // Assuming userId is the key in your JWT payload
+    req.user = await User.findById(decoded.userId); 
     if (!req.user) {
       return res.status(401).json({ message: "User not found" });
     }
